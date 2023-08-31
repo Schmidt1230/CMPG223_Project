fdc12b5885fd90fdfa07794bd75202afef2955b4
Wallstreet12 <stephanschmidt1230@gmail.com>
2023-08-31 21:37:16 +0200

    Started with Devices Form
    Started with some of the basic code on the devices form, mainly to interact with the form.
    
    Co-Authored-By: André Pretorius <143729669+AndreP04@users.noreply.github.com>
    Co-Authored-By: AspectY <142494363+AspectY@users.noreply.github.com>
    Co-Authored-By: DanniRoux <143721060+DanniRoux@users.noreply.github.com>
    Co-Authored-By: llewKrug <143730398+llewKrug@users.noreply.github.com>

---
+++ CMPG223_Project/frmDevices.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPG223_Project
+{
+    public partial class frmDevices : Form
+    {
+        public frmDevices()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNewDetails_TextChanged(object sender, EventArgs e)
+        {
+            int Characters = 100 - txtNewDetails.Text.Length;
+            lblWords.Text = "Characters left = "+Characters;
+
+            if(Characters<0)
+                lblWords.ForeColor= Color.Red;
+            else
+                lblWords.ForeColor= Color.LimeGreen;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdbUpdate_CheckedChanged(object sender, EventArgs e)
+        {
+            lblNavigate.Text = "Update Devices";
+            grpbxUpdate.Visible = true;
+            grpbxAddDevices.Visible = false;
+        }
+
+        private void rdbAdd_CheckedChanged(object sender, EventArgs e)
+        {
+            lblNavigate.Text = "Add Devices";
+            grpbxAddDevices.Visible = true;
+            grpbxUpdate.Visible = false;
+        }
+    }
+}
