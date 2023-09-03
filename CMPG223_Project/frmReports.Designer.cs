a3d24125cf81bbc4f0a4aa6af291621e317ddcdb
Wallstreet12 <stephanschmidt1230@gmail.com>
2023-09-03 19:21:32 +0200

    Added Report
    Finsihed Interface for the Report Interface.
    
    Co-Authored-By: AspectY <142494363+AspectY@users.noreply.github.com>
    Co-Authored-By: DanniRoux <143721060+DanniRoux@users.noreply.github.com>
    Co-Authored-By: André Pretorius <143729669+AndreP04@users.noreply.github.com>
    Co-Authored-By: llewKrug <143730398+llewKrug@users.noreply.github.com>

--- CMPG223_Project/frmReports.Designer.cs
+++ CMPG223_Project/frmReports.Designer.cs
@@ -30,29 +30,192 @@ namespace CMPG223_Project
         private void InitializeComponent()
         {
             this.pnlReports = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdbAscending = new System.Windows.Forms.RadioButton();
+            this.rdbDescending = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstReport = new System.Windows.Forms.ListBox();
+            this.btnDevice = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pnlReports.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlReports
             // 
+            this.pnlReports.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(69)))), ((int)(((byte)(79)))));
+            this.pnlReports.Controls.Add(this.groupBox2);
+            this.pnlReports.Controls.Add(this.groupBox1);
+            this.pnlReports.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlReports.Location = new System.Drawing.Point(0, 0);
             this.pnlReports.Name = "pnlReports";
-            this.pnlReports.Size = new System.Drawing.Size(1042, 184);
+            this.pnlReports.Size = new System.Drawing.Size(469, 528);
             this.pnlReports.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Light", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label1.Location = new System.Drawing.Point(773, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Generate a Report";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(203, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 35);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label2.Location = new System.Drawing.Point(26, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select a Technician";
+            // 
+            // rdbAscending
+            // 
+            this.rdbAscending.AutoSize = true;
+            this.rdbAscending.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbAscending.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.rdbAscending.Location = new System.Drawing.Point(187, 169);
+            this.rdbAscending.Name = "rdbAscending";
+            this.rdbAscending.Size = new System.Drawing.Size(126, 29);
+            this.rdbAscending.TabIndex = 3;
+            this.rdbAscending.TabStop = true;
+            this.rdbAscending.Text = "Ascending";
+            this.rdbAscending.UseVisualStyleBackColor = true;
+            // 
+            // rdbDescending
+            // 
+            this.rdbDescending.AutoSize = true;
+            this.rdbDescending.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbDescending.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.rdbDescending.Location = new System.Drawing.Point(30, 169);
+            this.rdbDescending.Name = "rdbDescending";
+            this.rdbDescending.Size = new System.Drawing.Size(137, 29);
+            this.rdbDescending.TabIndex = 4;
+            this.rdbDescending.TabStop = true;
+            this.rdbDescending.Text = "Descending";
+            this.rdbDescending.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label3.Location = new System.Drawing.Point(25, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(324, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Order By Repairs Completed";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(194, 92);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 31);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Show All";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDevice);
+            this.groupBox1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.groupBox1.Location = new System.Drawing.Point(19, 315);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(427, 184);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Devices Report";
+            // 
+            // lstReport
+            // 
+            this.lstReport.FormattingEnabled = true;
+            this.lstReport.ItemHeight = 16;
+            this.lstReport.Location = new System.Drawing.Point(528, 65);
+            this.lstReport.Name = "lstReport";
+            this.lstReport.Size = new System.Drawing.Size(478, 404);
+            this.lstReport.TabIndex = 1;
+            // 
+            // btnDevice
+            // 
+            this.btnDevice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDevice.Location = new System.Drawing.Point(40, 61);
+            this.btnDevice.Name = "btnDevice";
+            this.btnDevice.Size = new System.Drawing.Size(330, 76);
+            this.btnDevice.TabIndex = 0;
+            this.btnDevice.Text = "Most Repaired Device";
+            this.btnDevice.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdbAscending);
+            this.groupBox2.Controls.Add(this.rdbDescending);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Cascadia Code", 12F);
+            this.groupBox2.ForeColor = System.Drawing.Color.SpringGreen;
+            this.groupBox2.Location = new System.Drawing.Point(19, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(427, 227);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Technicians Report";
             // 
             // frmReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(69)))), ((int)(((byte)(79)))));
             this.ClientSize = new System.Drawing.Size(1042, 528);
+            this.Controls.Add(this.lstReport);
             this.Controls.Add(this.pnlReports);
+            this.Controls.Add(this.label1);
             this.Name = "frmReports";
             this.Text = "frmReports";
+            this.pnlReports.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel pnlReports;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton rdbAscending;
+        private System.Windows.Forms.RadioButton rdbDescending;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstReport;
+        private System.Windows.Forms.Button btnDevice;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
