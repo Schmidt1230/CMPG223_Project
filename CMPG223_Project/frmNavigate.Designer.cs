27b1814719503b955e2d03e02f7ee2e13b254546
DanniRoux <danikaleroux111@gmail.com>
2023-09-03 20:06:13 +0200

    Updated form + texboxes name

--- CMPG223_Project/frmNavigate.Designer.cs
+++ CMPG223_Project/frmNavigate.Designer.cs
@@ -31,14 +31,14 @@ namespace CMPG223_Project
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNavigate));
             this.pnlNavi = new System.Windows.Forms.Panel();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.btnTechnicians = new System.Windows.Forms.Button();
             this.btnInvoices = new System.Windows.Forms.Button();
             this.btnClients = new System.Windows.Forms.Button();
             this.btnDevices = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pnldashBoard = new System.Windows.Forms.Panel();
-            this.btnExit = new System.Windows.Forms.Button();
-            this.btnReports = new System.Windows.Forms.Button();
             this.pnlNavi.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -55,9 +55,41 @@ namespace CMPG223_Project
             this.pnlNavi.Controls.Add(this.pictureBox1);
             this.pnlNavi.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlNavi.Location = new System.Drawing.Point(0, 0);
+            this.pnlNavi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlNavi.Name = "pnlNavi";
-            this.pnlNavi.Size = new System.Drawing.Size(255, 669);
+            this.pnlNavi.Size = new System.Drawing.Size(191, 544);
             this.pnlNavi.TabIndex = 0;
+            // 
+            // btnReports
+            // 
+            this.btnReports.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReports.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.ForeColor = System.Drawing.Color.PaleGreen;
+            this.btnReports.Location = new System.Drawing.Point(0, 369);
+            this.btnReports.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(191, 58);
+            this.btnReports.TabIndex = 6;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.PaleGreen;
+            this.btnExit.Location = new System.Drawing.Point(0, 486);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(191, 58);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnTechnicians
             // 
@@ -66,9 +98,10 @@ namespace CMPG223_Project
             this.btnTechnicians.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTechnicians.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTechnicians.ForeColor = System.Drawing.Color.PaleGreen;
-            this.btnTechnicians.Location = new System.Drawing.Point(0, 385);
+            this.btnTechnicians.Location = new System.Drawing.Point(0, 311);
+            this.btnTechnicians.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnTechnicians.Name = "btnTechnicians";
-            this.btnTechnicians.Size = new System.Drawing.Size(255, 72);
+            this.btnTechnicians.Size = new System.Drawing.Size(191, 58);
             this.btnTechnicians.TabIndex = 4;
             this.btnTechnicians.Text = "Technicians";
             this.btnTechnicians.UseVisualStyleBackColor = true;
@@ -81,9 +114,10 @@ namespace CMPG223_Project
             this.btnInvoices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInvoices.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInvoices.ForeColor = System.Drawing.Color.PaleGreen;
-            this.btnInvoices.Location = new System.Drawing.Point(0, 313);
+            this.btnInvoices.Location = new System.Drawing.Point(0, 253);
+            this.btnInvoices.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnInvoices.Name = "btnInvoices";
-            this.btnInvoices.Size = new System.Drawing.Size(255, 72);
+            this.btnInvoices.Size = new System.Drawing.Size(191, 58);
             this.btnInvoices.TabIndex = 3;
             this.btnInvoices.Text = "Invoices";
             this.btnInvoices.UseVisualStyleBackColor = true;
@@ -96,9 +130,10 @@ namespace CMPG223_Project
             this.btnClients.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClients.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClients.ForeColor = System.Drawing.Color.PaleGreen;
-            this.btnClients.Location = new System.Drawing.Point(0, 241);
+            this.btnClients.Location = new System.Drawing.Point(0, 195);
+            this.btnClients.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClients.Name = "btnClients";
-            this.btnClients.Size = new System.Drawing.Size(255, 72);
+            this.btnClients.Size = new System.Drawing.Size(191, 58);
             this.btnClients.TabIndex = 2;
             this.btnClients.Text = "Clients";
             this.btnClients.UseVisualStyleBackColor = true;
@@ -111,9 +146,10 @@ namespace CMPG223_Project
             this.btnDevices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDevices.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDevices.ForeColor = System.Drawing.Color.PaleGreen;
-            this.btnDevices.Location = new System.Drawing.Point(0, 169);
+            this.btnDevices.Location = new System.Drawing.Point(0, 137);
+            this.btnDevices.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDevices.Name = "btnDevices";
-            this.btnDevices.Size = new System.Drawing.Size(255, 72);
+            this.btnDevices.Size = new System.Drawing.Size(191, 58);
             this.btnDevices.TabIndex = 1;
             this.btnDevices.Text = "Devices";
             this.btnDevices.UseVisualStyleBackColor = true;
@@ -126,8 +162,9 @@ namespace CMPG223_Project
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(255, 169);
+            this.pictureBox1.Size = new System.Drawing.Size(191, 137);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -137,50 +174,23 @@ namespace CMPG223_Project
             this.pnldashBoard.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnldashBoard.BackgroundImage")));
             this.pnldashBoard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pnldashBoard.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnldashBoard.Location = new System.Drawing.Point(255, 0);
+            this.pnldashBoard.Location = new System.Drawing.Point(191, 0);
+            this.pnldashBoard.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnldashBoard.Name = "pnldashBoard";
-            this.pnldashBoard.Size = new System.Drawing.Size(2575, 669);
+            this.pnldashBoard.Size = new System.Drawing.Size(1931, 544);
             this.pnldashBoard.TabIndex = 1;
-            // 
-            // btnExit
-            // 
-            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.ForeColor = System.Drawing.Color.PaleGreen;
-            this.btnExit.Location = new System.Drawing.Point(0, 597);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(255, 72);
-            this.btnExit.TabIndex = 5;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
-            // btnReports
-            // 
-            this.btnReports.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnReports.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReports.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReports.ForeColor = System.Drawing.Color.PaleGreen;
-            this.btnReports.Location = new System.Drawing.Point(0, 457);
-            this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(255, 72);
-            this.btnReports.TabIndex = 6;
-            this.btnReports.Text = "Reports";
-            this.btnReports.UseVisualStyleBackColor = true;
             // 
             // frmNavigate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1893, 669);
+            this.ClientSize = new System.Drawing.Size(1420, 544);
             this.Controls.Add(this.pnldashBoard);
             this.Controls.Add(this.pnlNavi);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmNavigate";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmNavigate";
+            this.Text = "Welcome to Alexander Tech Repairs";
             this.pnlNavi.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
